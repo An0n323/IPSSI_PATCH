2d9260ed78deb2b7c25163b0a9e90a8a58b0c8b8
Anon323 <mathys.m78@gmail.com>
2025-12-18 17:31:49 +0100

    modif route et sever

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/userConttroller'); // Attention à l'orthographe de ton fichier
 
+// Définition des routes
 router.get('/users', userController.getUsers);
+router.get('/populate', userController.populate);
 
 module.exports = router;
